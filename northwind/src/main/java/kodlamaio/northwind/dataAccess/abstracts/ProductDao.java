baa0c8d81f3c6a4fab393cb29e0d1dcb9010271e
Çetin Ürün <cetinurun.12@outlook.com>
2021-05-31 01:18:00 +0300

    Java camp day 8 codes.

--- northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
+++ northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -3,10 +3,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import kodlamaio.northwind.entities.concretes.Product;
+import kodlamaio.northwind.entities.dtos.ProductWithCategoryDto;
 
 public interface ProductDao extends JpaRepository<Product,Integer>{
 	
-Product getByProductName(String productName);
+	Product getByProductName(String productName);
 	
 	Product getByProductNameAndCategory_CategoryId(String productName, int categoryId);
 	
@@ -20,4 +21,8 @@ Product getByProductName(String productName);
 	
 	@Query("From Product where productName=:productName and category.categoryId=:categoryId")
 	List<Product> getByNameAndCategory(String productName, int categoryId);
+	
+	@Query("Select new kodlamaio.northwind.entities.dtos.ProductWithCategoryDto(p.id, p.productName, c.categoryName) "
+			+ "From Category c Inner Join c.products p")
+	List<ProductWithCategoryDto> getProductWithCategoryDetails();
 }
